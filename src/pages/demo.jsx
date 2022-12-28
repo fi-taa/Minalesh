9ccae40a541113629b32aa8038e0e8d76bd815fc
Yagth <yabuman46@gmail.com>
2022-12-28 10:16:01 +0300

    feat: import error

--- src/pages/demo.jsx
+++ src/pages/demo.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import SignUp from "../src/components/signUp";
-import Sudemo from "../src/components/sudemo";
-import Preview from "../src/components/general_res_preiview";
+import SignUp from "../components/signUp";
+import Sudemo from "../components/sudemo";
+import Preview from "../components/general_res_preiview";
 
 function demo() {
   return (
