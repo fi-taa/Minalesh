2b2802257df7c4388169141b4920e294384bc635
Yagth <yabuman46@gmail.com>
2023-01-04 09:57:13 +0300

    feat: compeleted user fetching function for the backend

--- src/models/user.js
+++ src/models/user.js
@@ -5,7 +5,7 @@ const prisma = new PrismaClient();
 export const createUserData = async (data) => {
   const { email, name, phoneNumber, role, password } = data;
   try {
-    const newUser = prisma.article.create({
+    const newUser = prisma.user.create({
       email,
       name,
       phoneNumber,
@@ -30,6 +30,83 @@ export const createUserData = async (data) => {
       };
     }
 
+    return { success: false, message: "Something went wrong" };
+  }
+};
+
+export const getUserData = async (id) => {
+  try {
+    const userData = await prisma.user.findUnique({
+      where: {
+        id,
+      },
+      select: {
+        id: true,
+        email: true,
+        name: true,
+        phoneNumber: true,
+        role: true,
+      },
+    });
+
+    return { success: true, message: { ...userData } };
+  } catch (error) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      return {
+        success: false,
+        message: "User doesn't exist",
+      };
+    }
+    return { success: false, message: "Something went wrong" };
+  }
+};
+
+export const updateUserData = async (data) => {
+  const { id, email, name, phoneNumber, password } = data;
+  try {
+    const updatedUser = await prisma.user.update({
+      where: { id },
+      data: {
+        email: email,
+        name: name,
+        phoneNumber: phoneNumber,
+        password: password,
+      },
+    });
+
+    const filteredUser = {
+      id: updatedUser.id,
+      email: updatedUser.email,
+      name: updatedUser.name,
+      phoneNumber: updatedUser.phoneNumber,
+    };
+
+    return { success: true, message: { ...filteredUser } };
+  } catch (error) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      return {
+        success: false,
+        message: "Couldn't update user due to invalid value",
+      };
+    }
+    return { success: false, message: "Something went wrong" };
+  }
+};
+
+export const deleteUserData = async (id) => {
+  try {
+    await prisma.user.delete({
+      where: { id },
+    });
+
+    return { success: true, message: "User deleted successfully" };
+  } catch (error) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      return {
+        success: false,
+        message: "Couldn't delete user due to database constraint",
+      };
+    }
     return { success: false, message: "Something went wrong" };
   }
 };
