ce5d7b334791bea537b66ad4a4c2c204e6997bc0
Yagth <yabuman46@gmail.com>
2023-01-04 10:47:00 +0300

    feat: added the function to update users

--- src/pages/api/_user.controller.js
+++ src/pages/api/_user.controller.js
@@ -1,4 +1,4 @@
-import { createUserData } from "../../models/user";
+import { createUserData, getUserData, updateUserData } from "../../models/user";
 
 export async function httpCreateUser(req, res) {
   const data = req.body;
@@ -13,4 +13,49 @@ export async function httpCreateUser(req, res) {
   return res
     .status(200)
     .json({ success: newUser.success, message: newUser.message });
+}
+
+export async function httpGetUser(req, res) {
+  const data = req.body;
+  const userData = await getUserData(data.id);
+
+  if (!userData.success) {
+    return res
+      .status(404)
+      .json({ success: userData.success, message: userData.message });
+  }
+
+  return res
+    .status(200)
+    .json({ success: userData.success, message: { ...userData } });
+}
+
+export async function httpUpdateUser(req, res) {
+  const data = req.body;
+  const updatedUser = await updateUserData(data);
+
+  if (!updateUserData.success) {
+    return res
+      .status(404)
+      .json({ success: updatedUser.success, message: updatedUser.message });
+  }
+
+  return res
+    .status(200)
+    .json({ success: updateUserData.success, message: updateUserData.message });
+}
+
+export async function httpDeleteUser(req, res) {
+  const data = req.body;
+  const message = await deleteUserData(data.id);
+
+  if (!message.success) {
+    return res
+      .status(404)
+      .json({ success: message.success, message: message.message });
+  }
+
+  return res
+    .status(200)
+    .json({ success: message.success, message: message.message });
 }
--- src/pages/api/user.js
+++ src/pages/api/user.js
@@ -1,7 +1,21 @@
-import { httpCreateUser } from "./_user.controller";
+import { httpCreateUser, httpUpdateUser } from "./_user.controller";
 
 export default function handler(req, res) {
-  if (req.method == "POST") {
-    httpCreateUser(req, res);
+  switch (req.method) {
+    case "GET":
+      httpGetUser(req, res);
+      break;
+    case "POST":
+      httpCreateUser(req, res);
+      break;
+    case "PATCH":
+      httpUpdateUser(req, res);
+      break;
+    case "DELETE":
+      httpDeleteUser(req, res);
+      break;
+    default:
+      res.status(404).end();
+      break;
   }
 }
